77e6cd076a2d4feeaded1b97f4499474374ae666
BrieucQ <brieucquertier73@gmail.com>
2022-02-23 16:35:20 +0100

    Page Home finalisé et lien en place avec le ticketDetails

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,95 @@
 import React from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
-
+import { View, ScrollView, Text, StyleSheet, Image } from "react-native";
+import { Card, Button, Icon } from 'react-native-elements';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Home () {
 
+  const navigation = useNavigation();
+
   return (
       <>
+      <ScrollView>
     <View style={styles.firstContainer}>
-    <Image style={styles.image} 
+      <View style={styles.headerContainer}>
+        <Image style={styles.image} 
         source={require('../assets/easy-ticket-logo.jpg')} 
         />
-        <Text style={styles.welcomeDev}>Welcome Dev</Text>
+        <Feather style={styles.feather} name="bell" size={18} color="black" />
+      </View>
+      <View style={styles.welcomeDev}>
+        <Text>Welcome Dev</Text>
         </View>
-        <View style={styles.secondContainer}>
-      <Text>Details screen</Text>
     </View>
+    <View style={styles.secondContainer}>
+    <Card>
+            <Card.Title>PROJET 1</Card.Title>
+            <Card.Divider />
+            <Text style={{ marginBottom: 10 }}>
+              Ticket 1 : Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Text>
+            <Card.Divider />
+            <Text style={{ marginBottom: 10 }}>
+              Ticket 2 : Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Text>
+            <Button
+              icon={
+                <Icon
+                  name="code"
+                  color="#ffffff"
+                  iconStyle={{ marginRight: 10 }}
+                />
+              }
+              buttonStyle={{
+                borderRadius: 0,
+                marginLeft: 0,
+                marginRight: 0,
+                marginBottom: 0,
+              }}
+              title="VOIR PLUS"
+              onPress={() => navigation.navigate('TicketDetails')}
+            />
+          </Card>
+          <Card>
+            <Card.Title>PROJET 1</Card.Title>
+            <Card.Divider />
+            <Text style={{ marginBottom: 10 }}>
+              Ticket 1 : Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Text>
+            <Card.Divider />
+            <Text style={{ marginBottom: 10 }}>
+              Ticket 2 : Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Text>
+            <Button
+              icon={
+                <Icon
+                  name="code"
+                  color="#ffffff"
+                  iconStyle={{ marginRight: 10 }}
+                />
+              }
+              buttonStyle={{
+                borderRadius: 0,
+                marginLeft: 0,
+                marginRight: 0,
+                marginBottom: 0,
+              }}
+              title="VOIR PLUS"
+              onPress={() => navigation.navigate('TicketDetails')}
+            />
+          </Card>
+    </View>
+    </ScrollView>
     </>
   );
 }
 
 const styles = StyleSheet.create({
     firstContainer: {
-        flex: 0.2,
-        justifyContent: "center",
+      flexDirection: "row",
+      flex: 0.25,
+      justifyContent: "space-between"
     },
   secondContainer: {
     flex: 2,
@@ -30,16 +97,20 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  headerContainer: {
+    flexDirection: "row",
+    alignContent: "space-between",
+    margin: 10
+  },
   welcomeDev: {
-      alignSelf: "flex-end",
-      marginRight: 15
+      marginRight: 15,
+      alignSelf: "center",
   },
   image: {
       alignSelf: "flex-start",
       justifyContent: 'center',
-      position: "absolute",
       height: 50,
       width: 50,
       margin: 5
-  }
+  },
 });
